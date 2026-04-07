035cd8b394ea18d03440a241b0dd093838940e96
sshreve <sshreve@babtel.net>
2026-04-07 12:08:15 -0400

    settings db options

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -5,6 +5,9 @@
 
     let toggled = $state(false);
 
+    let extdbstate = $state(false);
+    let localDbCreated = $state(false);
+
 </script>
 
 <main class="settings-container">
@@ -12,12 +15,30 @@
         <div class="settings-info">
             <h4 class="setting-title"> Data Storage Settings</h4>
             <div class="settings-option">
-                <p class="setting-desc"> Use Organization DB</p>
-                <ToggleButton isToggled={toggled} clickAction={() => toggled = !toggled} />
+                <p class="setting-desc"> Use External database</p>
+                <ToggleButton isToggled={extdbstate} clickAction={() => extdbstate = !extdbstate} />
             </div>
             <div class="settings-option">
-                <p class="setting-desc"> Setting description</p>
-                <TextButton text="Create Local DB" kind="bright" size="small" />
+                {#if !extdbstate}
+                    <p class="setting-desc"> Set Up Local database</p>
+                    {#if !localDbCreated}
+                        <TextButton text="Create Local DB" kind="bright" size="small" clickAction={() => localDbCreated = true} />
+                    {:else}
+                        <div class="settings-option-btns">
+                            <TextButton text="Wipe Local DB" kind="bright" size="small" />
+                            <TextButton text="Remove Local DB" kind="bright" size="small" clickAction={() => localDbCreated = false}/>
+                        </div>
+                    {/if}
+                {:else}
+                    <p class="setting-desc"> Set Up External database</p>
+                    <div class="db-form-grid">
+                        <input type="text" placeholder="Host" />
+                        <input type="text" placeholder="Port" />
+                        <input type="text" placeholder="User" />
+                        <input type="text" placeholder="Password" />
+                        <input type="text" placeholder="Database" />
+                    </div>
+                {/if}
             </div>
         </div>
     </div>
